408e2aec3575c43d8e852c9f7118f54e7e354918
sabyasachi <sabyasachikol@gmail.com>
2022-05-03 13:38:14 +0530

    Reverse a string

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Reverse a string -Program 1
+string mystring = "hello";
+char[] c= mystring.ToCharArray();
+string result = string.Empty;
+for (int i = c.Length; i > 0; i--)
+{
+    result+=c[i-1].ToString();// as array index starts with zero hence i-1 would be 5th index
+}
+
+Console.WriteLine(result);
